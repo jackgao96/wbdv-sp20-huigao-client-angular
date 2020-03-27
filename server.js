0315bd1dce42714473a7235aa8cb602538b47571
jackgao118 <gao.hui1@husky.neu.edu>
2020-03-27 18:16:44 -0400

    update angular

--- server.js
+++ server.js
@@ -2,7 +2,5 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/wbdv-sp20-huigao-client-angular'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/wbdv-sp20-huigao-client-angular/index.html'));});
+app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/wbdv-sp20-huigao-client-angular/index.html'));});
 app.listen(process.env.PORT || 8080);
